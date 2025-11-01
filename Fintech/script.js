4fc1e14db8fd396d3fcabe017a753e9e20dadd1a
Nicole Dias <nicolesilvadias27@gmail.com>
2025-10-31 21:22:59 -0300

    Update script.js

---
+++ Fintech/script.js
@@ -0,0 +1,23 @@
+//Iniciação da página
+document.addEventListener('DOMContentLoaded', ()=>{
+
+const dadosExtrato ={
+    saldo:4820.90,
+    transacoes:[
+        {tipo:'venda', descricao:'Venda - Rímel', valor:150.50},
+        {tipo:'venda', descricao:'Venda - Blush', valor:50.20},
+        {tipo:'pix', descricao:'Pagamento ao fornecedor - GlossBeauty', valor:-300.00},
+        {tipo:'cashback', descricao:'Cashback - campanha de ações do GlossBeauty', valor:100.00},
+        {tipo:'compra', descricao:'compra - Estoque de base da RareBeauty', valor:-3160.90},
+        {tipo:'taxa', descricao:'Taxa - Importação de mercadoria', valor:-200.00},
+        {tipo:'venda', descricao:'Venda - Kit de maguiagens importados', valor:3999.99},
+        {tipo:'pix', descricao:'Recebido via pix - Cliente Daniela Perez', valor:1500.50},
+        {tipo:'reembolso', descricao:'Reembolso cliente - Pedido #A3490', valor:-89.00},
+        {tipo:'Serviço', descricao:'Assinatura Beauty+ (Mensal)', valor:-51.29},
+    ]
+
+};
+
+
+
+})
