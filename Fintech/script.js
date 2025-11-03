4b2e673831485fb6b85889084792489a7ee24dcf
Nicole Dias <nicolesilvadias27@gmail.com>
2025-11-03 19:14:14 -0300

    update

--- Fintech/script.js
+++ Fintech/script.js
@@ -10,14 +10,48 @@ const dadosExtrato ={
         {tipo:'cashback', descricao:'Cashback - campanha de ações do GlossBeauty', valor:100.00},
         {tipo:'compra', descricao:'compra - Estoque de base da RareBeauty', valor:-3160.90},
         {tipo:'taxa', descricao:'Taxa - Importação de mercadoria', valor:-200.00},
-        {tipo:'venda', descricao:'Venda - Kit de maguiagens importados', valor:3999.99},
-        {tipo:'pix', descricao:'Recebido via pix - Cliente Daniela Perez', valor:1500.50},
+        {tipo:'venda', descricao:'Venda - Kit de maguiagens importados', valor:5999.99},
+        {tipo:'pix', descricao:'Recebido via pix - Cliente Daniela Perez', valor:8500.50},
         {tipo:'reembolso', descricao:'Reembolso cliente - Pedido #A3490', valor:-89.00},
-        {tipo:'Serviço', descricao:'Assinatura Beauty+ (Mensal)', valor:-51.29},
+        {tipo:'Serviço', descricao:'Assinatura Beauty+ (Mensal)', valor:-51.29}
     ]
 
 };
 
+const saldoElemento = document.getElementById("saldo-conta");
+const listaTransacoes = document.getElementById("lista-transacoes");
 
+//chamada do extrato
+saldoElemento.textContent = `Saldo ${dadosExtrato.saldo.toLocaleString('pt-BR',{
+    style: 'currency',
+    currency: 'BRL'
+})}`;
 
-})
+//separando cada transação
+dadosExtrato.transacoes.forEach(transacao=>{
+    const itemLista = document.createElement('li');
+    itemLista.classList.add('transacao');
+
+    if (Math.abs(transacao.valor) >= 5000) {
+        itemLista.classList.add('destaque');
+    }
+
+    const ValorFormatado = new Intl.NumberFormat('pt-BR',{
+        style: 'currency',
+        currency: 'BRL'
+    }).format(transacao.valor);
+
+    itemLista.innerHTML =`
+    <div class="transacao-info">
+        <span> ${transacao.descricao}</span>
+        <small> ${transacao.valor}</small>
+        <span class="transacao-valor ${transacao.valor > 0 ? 'negativo' : ''}" >
+            ${ValorFormatado}
+        </span>
+
+    `;
+
+    listaTransacoes.appendChild(itemLista);
+});
+
+});
